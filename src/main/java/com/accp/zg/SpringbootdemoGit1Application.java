1e8ac483c8f30880b55f5fbc4469d9c63d6e3d0b
zhougang <948837561@qq.com>
2018-10-17 10:57:48 +0800

    1222424

---
+++ src/main/java/com/accp/zg/SpringbootdemoGit1Application.java
@@ -0,0 +1,12 @@
+package com.accp.zg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoGit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoGit1Application.class, args);
+	}
+}
